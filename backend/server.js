9c634cdc276f7b0d3b41df7264f094a9e40f46e7
ashu-1403 <129203568+ashu-1403@users.noreply.github.com>
2025-01-02 22:01:57 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
 import express from 'express';
+import userRoutes from './routes/user.route.js';
+
+
+const app= express();
+
+app.get("/", (req,res)=>{
+    res.send("API is running")
+})
+
+app.use("/api/users",userRoutes)
+
+app.listen(5000, ()=>{
+    console.log("server started on the port 5000")
+})
